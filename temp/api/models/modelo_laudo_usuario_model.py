6690a2ff7279378020a647b676573cd05d948194
Hudsonrb92 <hudson.rabeloborges@gmail.com>
2020-12-04 11:59:08 -0300

    Removido temp

--- temp/api/models/modelo_laudo_usuario_model.py
+++
@@ -1,9 +0,0 @@
-from api import db
-
-
-class ModeloLaudoUsuarioModel(db.Model):
-    __table_args__ = {"schema": "public"}
-    __tablename__ = "modelo_laudo_usuario"
-
-    identificador_modelo_laudo = db.Column(db.Integer, nullable=False, primary_key=True)
-    identificador_usuario = db.Column(db.Integer, nullable=False)
