c87eee28318b6d70b179dd99e01e87d7e6c9a805
hudsonrb92 <hudson.rabeloborges@gmail.com>
2020-12-09 13:57:11 -0300

    Alterado função de busca de laudos.

--- api/services/laudo_estudo_dicom_service.py
+++ api/services/laudo_estudo_dicom_service.py
@@ -1,25 +1,25 @@
 from api import db
 from ..models.laudo_estudo_dicom_model import LaudoEstudoDicomModel as lem
-from ..models.estudo_dicom_model import EstudoDicomModel
+from ..models.integracao_tasy_model import IntegracaoTasyModel as itm
+from ..models.estudo_dicom_model import EstudoDicomModel as edm
 from base64 import b64encode
 
 
 def listar_laudos(identificador_estabelecimento_saude):
-    estudos = (
-        lem.query.filter_by(integrado=False)
-        .join(EstudoDicomModel)
-        .filter(
-            EstudoDicomModel.identificador_estabelecimento_saude == identificador_estabelecimento_saude,
-            EstudoDicomModel.situacao == "V",
-            EstudoDicomModel.accessionnumber != None,
+    sttmnt = (
+        lem.query.filter(
+            lem.identificador_estudo_dicom == itm.identificador_estudo_dicom,
+            lem.identificador_estudo_dicom == edm.identificador,
         )
-        .all()
+        .filter(edm.accessionnumber != None)
+        .filter(lem.integrado == False)
     )
+    estudos = sttmnt.all()
     return estudos
 
 
 def listar_estudo_por_id(identificador):
-    laudo = lem.query.filter_by(identificador=identificador).join(EstudoDicomModel).first()
+    laudo = lem.query.filter_by(identificador=identificador).join(edm).first()
     return laudo
 
 
@@ -31,7 +31,7 @@ def update_to_integrado(laudo):
 def get_pdf(identificador: int) -> b64encode:
     laudo = lem.query.filter_by(identificador=identificador).first()
     if not laudo:
-        raise Exception(f"Error, laudo not found")
+        raise Exception("Error, laudo not found")
     pdf_name = f"{laudo.identificador}-{laudo.data_hora_emissao}"
     dir_name = "/data/integracao/laudos/"
 
